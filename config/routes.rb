05f6bbbbfddbfa0057e71ebe510ac97a17023dff
AntoineBoumBoum <haffreinguea+github@gmail.com>
2024-02-20 16:58:37 +0100

    made add, return to home, display

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  get "/articles", to: "articles#index"
+  get "articles", to: "articles#index"
+  get "articles/new", to: "articles#new"
+  post "articles", to: "articles#create"
+  patch "articles", to:"article#uptade"
+  delete "articles/:id", to: "articles#destroy"
+  get "articles/:id/edit", to: "articles#edit"
+  get "articles/:id", to: "articles#show"
 end
